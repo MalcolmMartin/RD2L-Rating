4ac2d19ed7f63fd6c789d983ed27ef63f122c586
Malcolm Martin <mn2marti@uwaterloo.ca>
2022-06-01 17:14:53 -0400

    Dotabuff Stats Bug Fix
    Fixed a Dotabuff lane stats bug that resulted in incorrect hero-lane expectations and reformatted that .py file

--- src/OriginalLoop.py
+++ src/OriginalLoop.py
@@ -6,64 +6,77 @@ import json
 from bs4 import BeautifulSoup
 import math
 
-def makestats(lane):
-    r = requests.get('https://www.dotabuff.com/heroes/lanes?lane='+lane, headers={'user-agent': 'Mozilla/5.0'})
-    soup = BeautifulSoup(r.content, 'lxml')
-    soup.find_all(class_="sortable")
+def make_lane_stats_df(lane):
+    # Request the dotabuff lanes page for given lane and soupify
+    r = requests.get('https://www.dotabuff.com/heroes/lanes?lane=' + lane,
+                     headers={'user-agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(r.content)
 
-    # #Processing table w/ BS 
-    table = soup ('table')[0]
-    headers = [th.text for th in table ('th')] #skipping garbage to grab all the text header guys
-    rows = [x[1:] for x in [ [ td.text for td in tr('td') ] for tr in table.tbody('tr') ]]
-
-
-    stats   = [ dict(zip(headers, row)) for row in rows ]
-    #making dictionary then exporting it to json
-
-    stats_dataframe = pandas.concat((pandas.json_normalize(d) for d in stats), axis=0)
-    #stats_dataframe.to_csv(lane+'Stats.csv', index=False, encoding='utf-8')
-
+    # Process table from html to JSON to dataframe
+    table = soup('table')[0]
+    headers = [th.text for th in table ('th')]
+    rows = [x[1:] for x in
+            [[td.text for td in tr('td')] for tr in table.tbody('tr')]]
+    stats = [dict(zip(headers, row)) for row in rows]
+    
+    # Making dictionary then exporting it to json
+    stats_df = pandas.concat((pandas.json_normalize(d) for d in stats), axis=0)
     hero_ids = pandas.read_csv("Hero_Ids.csv")
 
-    #print (hero_ids, stats_dataframe)
-    #read all the rows in hero_ids and then see if stats dataframe has the hero and if it doesnt then add a placeholder 
-    #print(stats_dataframe)
+    # Cross reference stats dataframe and hero ids
+    # Add a placeholder if hero isn't in the dataframe
     for index, row in hero_ids.iterrows():
-        #print(row ['hero_id'], row['hero_name'])
-        if not stats_dataframe['Hero'].str.contains(row['hero_name']).any():
-            # a_row = pandas.Series([row['hero_name'],'a', 'b', 'c', 'd', 'e'])
-            # row_df = pandas.DataFrame([a_row])
-            #stats_dataframe = pandas.concat([row_df, stats_dataframe], ignore_index=True)
-            stats_dataframe = stats_dataframe.append({'Hero' : row['hero_name']} , ignore_index=True)
+        if not stats_df['Hero'].str.contains(row['hero_name']).any():
+            stats_df = stats_df.append({'Hero':row['hero_name']},
+                                       ignore_index=True)
 
-    return(stats_dataframe)
+    return(stats_df)
 
 def get_dotabuff_hero_lane_data():
-    #adding an extra column for lanes - ordering lanes to match up with positions 
+    # Adding an extra column for lanes to match up with positions 
     lanes = ['safe', 'mid', 'off', 'off', 'safe', 'jungle', 'roaming']
     positions = [1, 2, 3, 4, 5, 6, 7]
 
-    statsbylane  = pandas.DataFrame()
+    # Gather all the lane stats into a single dataframe
+    all_lane_stats_df = pandas.DataFrame()
     for lane, position in zip(lanes, positions):
-        test = makestats(lane)
-        df = pandas.DataFrame(test)
-        df['Roles'] = position
-        statsbylane = pandas.concat([statsbylane, test])
+        lane_stats_df = make_lane_stats_df(lane)
+        lane_stats_df['Roles'] = position
+        all_lane_stats_df = pandas.concat([all_lane_stats_df, lane_stats_df])
+    
+    # Reset index values since combining the dataframes resulted in multiple
+    # rows for each index value (i.e. 7 different rows with index 1)
+    all_lane_stats_df = all_lane_stats_df.reset_index()
+    del all_lane_stats_df['index']
+    
+    # Convert data columns to floats
+    all_lane_stats_df['Presence'] = \
+        all_lane_stats_df['Presence'].str.rstrip('%').astype('float') / 100.0
+    all_lane_stats_df['Win Rate'] = \
+        all_lane_stats_df['Win Rate'].str.rstrip('%').astype('float') / 100.0
+    all_lane_stats_df['KDA Ratio'] = \
+        all_lane_stats_df['KDA Ratio'].astype('float')
+    all_lane_stats_df['GPM'] = all_lane_stats_df['GPM'].astype('float')
+    all_lane_stats_df['XPM'] = all_lane_stats_df['XPM'].astype('float')
 
-    #convert columns to non-percentage FLOATy bOIS
-    statsbylane['Presence'] = statsbylane['Presence'].str.rstrip('%').astype('float') / 100.0
-    statsbylane['Win Rate'] = statsbylane['Win Rate'].str.rstrip('%').astype('float') / 100.0
-    statsbylane['KDA Ratio'] = statsbylane['KDA Ratio'].astype('float')
-    statsbylane['GPM'] = statsbylane['GPM'].astype('float')
-    statsbylane['XPM'] = statsbylane['XPM'].astype('float')
-
-    for index, row in statsbylane.iterrows():
+    # Replace placeholder stats with the average of other lanes
+    final_lane_stats_df = all_lane_stats_df
+    for index, row in final_lane_stats_df.iterrows():
         if math.isnan(row['Presence']):
-            statsbylane.at[index, 'Presence'] = 0
-            statsbylane.at[index, 'Win Rate'] = statsbylane[statsbylane.Hero == row['Hero']]['Win Rate'].mean()
-            statsbylane.at[index, 'KDA Ratio'] = statsbylane[statsbylane.Hero == row['Hero']]['KDA Ratio'].mean()
-            statsbylane.at[index, 'GPM'] = statsbylane[statsbylane.Hero == row['Hero']]['GPM'].mean()
-            statsbylane.at[index, 'XPM'] = statsbylane[statsbylane.Hero == row['Hero']]['XPM'].mean()
+            final_lane_stats_df.at[index, 'Presence'] = 0
+            final_lane_stats_df.at[index, 'Win Rate'] = \
+                all_lane_stats_df[all_lane_stats_df.Hero ==
+                                  row['Hero']]['Win Rate'].mean()
+            final_lane_stats_df.at[index, 'KDA Ratio'] = \
+                all_lane_stats_df[all_lane_stats_df.Hero ==
+                                  row['Hero']]['KDA Ratio'].mean()
+            final_lane_stats_df.at[index, 'GPM'] = \
+                all_lane_stats_df[all_lane_stats_df.Hero ==
+                                  row['Hero']]['GPM'].mean()
+            final_lane_stats_df.at[index, 'XPM'] = \
+                all_lane_stats_df[all_lane_stats_df.Hero ==
+                                  row['Hero']]['XPM'].mean()
 
-    statsbylane.to_csv('statsbylane.csv', index=False, encoding='utf-8')
+    # Save the results to csv for future reference
+    final_lane_stats_df.to_csv('statsbylane.csv', index=False, encoding='utf-8')
 
